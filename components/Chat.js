4521a087460add4c43eed423377970359b375128
ffernandez524 <flareniel@gmail.com>
2023-07-02 12:48:13 -0400

    Added GiftedChat module and speech bubbles

--- components/Chat.js
+++ components/Chat.js
@@ -1,19 +1,67 @@
-import { StyleSheet, View, Text } from 'react-native';
-import { useEffect } from 'react';
+import { StyleSheet, View, KeyboardAvoidingView } from 'react-native';
+import { useState, useEffect } from 'react';
+import { Bubble, GiftedChat } from 'react-native-gifted-chat';
 
+//Chat Screen
 const Chat = ({ route, navigation }) => {
-  const { name } = route.params;
-  const { chosenColor } = route.params;
-
+  const { name, chosenColor } = route.params;
+  const [messages, setMessages] = useState([]);
+  
+  //Initial Messages when app loads
   useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: 'Hello developer',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'React Native',
+          avatar: 'https://placeimg.com/140/140/any'
+        },
+      },
+      {
+        _id: 2,
+        text: 'This is a system message',
+        createdAt: new Date(),
+        system: true,
+      },
+    ]);
     navigation.setOptions({ 
       title: name 
     });
   }, []);
 
+  const onSend = (newMessages) => {
+    setMessages(previousMessages => GiftedChat.append(previousMessages, newMessages));
+  }
+
+  //Speech bubble and colors
+  const renderBubble = (props) => {
+    return <Bubble
+      {...props}
+      wrapperStyle={{
+        right: {
+          backgroundColor: '#000'
+        },
+        left: {
+          backgroundColor: '#FFF'
+        }
+      }}
+    />
+  }
+
   return (
     <View style={[styles.container, { backgroundColor: chosenColor }]}>
-      <Text style={styles.title}>Time to Chat!</Text>
+      <GiftedChat
+        messages={messages}
+        renderBubble={renderBubble}
+        onSend={messages => onSend(messages)}
+        user={{
+          _id: 1
+        }}
+      />
+      { Platform.OS === 'android' ? <KeyboardAvoidingView behavior='height'/> : null}
     </View>
   );
 }
@@ -21,15 +69,6 @@ const Chat = ({ route, navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  title: {
-    flex: 1,
-    margin: '15%',
-    fontSize: 45,
-    fontWeight: 600,
-    color: '#FFFFFF'
   },
 });
 
